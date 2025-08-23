01db82e19c1c05ba806dd6222a81c24b1b8e7825
kononovs457-wq <kononovs457@gmail.com>
2025-08-23 12:00:42 +0200

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -12,18 +12,16 @@ int main ()
 double a = 0, b = 0, c = 0;
 double x1 = 0, x2 = 0;
 scanf("%lg %lg %lg", &a, &b, &c);
-printf ("%g /n, %g /n , %g /n,", *x1, *x2);
+printf ("%lf /n, %lf /n", &x1, &x2);
 }
 
 double solve_equations (double a, double b, double c, double *x1, double *x2)
 {
-  double d;
-  d = 0;
-  d = b*b - 4*a*c;
+  int d = b*b - 4*a*c;
   if ( a == 0) {
       if ( b == 0)  {
           if ( c == 0) {
-              return INF: 0 /* проверка на бесконечное колво решений*/
+              return INF: 0; /* ГЇГ°Г®ГўГҐГ°ГЄГ  Г­Г  ГЎГҐГ±ГЄГ®Г­ГҐГ·Г­Г®ГҐ ГЄГ®Г«ГўГ® Г°ГҐГёГҐГ­ГЁГ©*/
 }
 }
 }
@@ -32,16 +30,13 @@ double solve_equations (double a, double b, double c, double *x1, double *x2)
        *x2 = ( - b - sqrt(d) ) / (2*a);
        return 2;
 }
-  if (d = 0) {
-       *x1 = *x2 = - b / (2 * a)
+  if (d == 0) {
+       *x1 = *x2 = - b / (2 * a);
        return 1;
      }
  if (d < 0) {
      printf ( "This equations haven't any solves");
      return 0;
- } /* решение самого уравнения*/
+ } /* Г°ГҐГёГҐГ­ГЁГҐ Г±Г Г¬Г®ГЈГ® ГіГ°Г ГўГ­ГҐГ­ГЁГї*/
 }
- int main ()
- {
-   printf ("%g /n, %g /n , %g /n,", *x1, *x2, numberofroots);
  }
