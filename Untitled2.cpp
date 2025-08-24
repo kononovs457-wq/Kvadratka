43df0be1d2f1bbe7d87aa82005adc67074711877
kononovs457-wq <kononovs457@gmail.com>
2025-08-24 11:24:28 +0200

    Update Untitled2.cpp

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -13,7 +13,7 @@ int main()
     double a = 0, b = 0, c = 0;
     Test_Solve();
 	FILE *myfile;
-	myfile = fopen(
+	myfile = fopen("file.txt", "r")
     scanf ("%lg %lg %lg", &a, &b, &c);
     double x1 = 0, x2 = 0;
     int nRoots = SolveSquare (a, b, c, &x1, &x2);
